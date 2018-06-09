cbeb1410ff335247f2703d7738b56fe99a15dde4
endwith <1813214042@qq.com>
2018-06-09 10:14:57 +0800

    spring-cloud简单完成框架

--- springcloud-eureka-cluster/spring-conf-client/src/main/java/com/example/springconfclient/SpringConfClientApplication.java
+++ springcloud-eureka-cluster/spring-conf-client/src/main/java/com/example/springconfclient/SpringConfClientApplication.java
@@ -3,11 +3,13 @@ package com.example.springconfclient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 @EnableEurekaServer
 @SpringBootApplication
+@RefreshScope
 @RestController
 public class SpringConfClientApplication {
 
--- springcloud-eureka-cluster/spring-conf-server/src/main/java/com/example/springconfserver/SpringConfServerApplication.java
+++ springcloud-eureka-cluster/spring-conf-server/src/main/java/com/example/springconfserver/SpringConfServerApplication.java
@@ -2,9 +2,11 @@ package com.example.springconfserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
+@EnableConfigServer  //conf-server 启动注解
 @SpringBootApplication
 public class SpringConfServerApplication {
 
